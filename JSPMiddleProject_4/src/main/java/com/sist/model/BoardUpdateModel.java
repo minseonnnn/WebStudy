07545f5d100917abaf84735e70833597af5a98a6
minseonnnn <mm1687d@naver.com>
2024-07-19 17:10:10 +0900

    0719

---
+++ JSPMiddleProject_4/src/main/java/com/sist/model/BoardUpdateModel.java
@@ -0,0 +1,21 @@
+package com.sist.model;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.sist.dao.BoardDAO;
+import com.sist.dao.BoardVO;
+
+public class BoardUpdateModel implements Model {
+
+	@Override
+	public String execute(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		// update.jsp?no=1
+		   String no=request.getParameter("no");
+		   BoardDAO dao=BoardDAO.newInstance();
+		   BoardVO vo=dao.boardUpdateData(Integer.parseInt(no));
+		   request.setAttribute("vo", vo);
+		return "update.jsp";
+	}
+
+}
