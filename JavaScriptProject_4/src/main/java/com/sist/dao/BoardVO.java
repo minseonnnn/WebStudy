dc9617ac1e3cd82e8091a34625e34506f0ada02e
minseonnnn <mm1687d@naver.com>
2024-08-01 15:47:38 +0900

    0731

---
+++ JavaScriptProject_4/src/main/java/com/sist/dao/BoardVO.java
@@ -0,0 +1,21 @@
+package com.sist.dao;
+/*
+ *   1. 목록 => 페이징
+ *   2. 상세보기
+ *   3. CRUD => 게시판 
+ *   ================= 신입사원 요구사항 
+ *   MVC / Spring 
+ *   
+ *   => JSON => Ajax / Vue / React
+ *   => AWS : 호스팅 
+ */
+import java.util.*;
+
+import lombok.Data;
+@Data
+public class BoardVO {
+	  private int no, hit;
+	  private String name, subject, content, pwd, dbday;
+	  private Date regdate;
+	  private int rownum;
+}
